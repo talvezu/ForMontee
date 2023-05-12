60129bb8276a3896f334732ff64a59a5a4fe8919
talvezu <talvezu@walla.com>
2023-05-12 08:29:13 +0300

    WIP, callbacks for setting values in db

--- app/InteractiveTask.h
+++ app/InteractiveTask.h
@@ -24,29 +24,28 @@ using thread_safe_sample_queue = threadsafe_queue<sample_data>;
 using shared_ptr_thread_safe_sample_queue = std::shared_ptr<threadsafe_queue<sample_data>>;
 
 //using queue_map = std::map<std::string, std::pair<threadsafe_queue<sample_data>, threadsafe_queue<sample_data>>>;
-//template<class T> to do
+template<class K, class V>
 class InteractiveTask{
     std::string task_name;
-    shared_ptr_thread_safe_sample_queue &in;
-    shared_ptr_thread_safe_sample_queue &out;
     std::unique_ptr<PQueue<PriorityQueueElement>> inner_queue;
     std::shared_ptr<std::atomic<bool>> end_task;
     int milli_interval;
-
     std::shared_ptr<TaskControlBlock<float>> task_control_block;
     std::shared_ptr<PeriodicTask<float>> periodic_task;
-
-    std::function<float(uint32_t, float)> callback;
+    std::function<float(uint32_t, float)> process_net_functions_callback;
+    std::function<std::map<K, V>(const std::string &)> get_new_data_func;
+    std::function<void(const std::string , std::map<K, V>)> set_new_data_func;
 public:
 
     InteractiveTask(std::string &&_task_name,
-                    shared_ptr_thread_safe_sample_queue &_in,
-                    shared_ptr_thread_safe_sample_queue &_out,
                     std::unique_ptr<PQueue<PriorityQueueElement>> _inner_queue,
                     std::shared_ptr<std::atomic<bool>> _end_task,
                     int _milli_interval,
-                    std::function<float(uint32_t, float)> _callback)
-                    : task_name(_task_name), in(_in), out(_out),
+                    std::function<float(uint32_t, float)> _process_net_functions_callback,
+                    std::function<std::map<K, V>(const std::string &)> _get_new_data_func = nullptr,
+                    std::function<void(const std::string , std::map<K, V>)> _set_new_data_func = nullptr
+                    )
+                    : task_name(_task_name),
                     inner_queue(std::move(_inner_queue)), end_task(_end_task), milli_interval(_milli_interval),
                     task_control_block(std::make_shared<TaskControlBlock<float>>(
                                        end_task,
@@ -57,10 +56,21 @@ public:
                                        std::bind(&InteractiveTask::read, this, std::placeholders::_1),
                                        std::bind(&InteractiveTask::write, this, std::placeholders::_1))),
                     periodic_task(std::make_shared<PeriodicTask<float>>(task_control_block)),
-                    callback(_callback)
+                    process_net_functions_callback(_process_net_functions_callback),
+                    get_new_data_func(_get_new_data_func),
+                    set_new_data_func(_set_new_data_func)
+
     {
 
     }
+
+    InteractiveTask(std::function<void(const std::string , std::map<K, V>)> _set_new_data_func = nullptr)
+                    : set_new_data_func(_set_new_data_func)
+
+    {
+
+    }
+
 
 	void read(std::shared_ptr<float> &Entry)
     {
@@ -82,35 +92,8 @@ public:
                     std::cout<<*elem<<"\n";
                 for (auto &motor: elem->target_motors)
                 {
-                    out->push({static_cast<uint32_t>(motor), value});
+                    //out->push({static_cast<uint32_t>(motor), value});
                 }
-
-            }
-        }
-
-        //read from input queue
-        while( true )
-        {
-            auto elem = in->try_pop();
-            if (!elem)
-                break;
-
-            float value = elem->value;
-            if (motors_from_inner_queue_value.find(elem->motor_id) == motors_from_inner_queue_value.end())
-            {
-                if (logging::active)
-                    std::cout<<"motor: "<<elem->motor_id<<" value:"<<value<<"\n";
-                float val_returned = value;
-                if (callback)
-                    val_returned = callback(elem->motor_id, value);
-
-                std::cout<<"motor: "<<elem->motor_id<<" value retured:"<<val_returned<<"\n";
-                out->push({static_cast<uint32_t>(elem->motor_id), val_returned});
-            }
-            else
-            {
-                //if (logging::active)
-                    std::cout<<"skipped! motor: "<<elem->motor_id<<" value:"<<value<<"\n";
 
             }
         }
@@ -127,12 +110,5 @@ public:
         //optional write to future db;
     }
 
-    void inject_elements_to_queue(const std::set<uint32_t> motors)
-    {
-        for (auto motor: motors)
-        {
-            in->push({motor, 0.5f});
-        }
-    }
 };
 
--- app/TasksFromConfig.cpp
+++ app/TasksFromConfig.cpp
@@ -5,6 +5,7 @@
 #include "PriorityQueueElement.h"
 #include "NetFunctionFactory.h"
 #include "InteractiveTask.h"
+#include "DB.h"
 #include "logging.h"
 
 
@@ -22,7 +23,7 @@ engine<float>;
 
 template <class T>
 std::map<std::string, engine<T>> engines_map;
-std::map<std::string, shared_ptr<InteractiveTask>> InteractiveTasks;
+std::map<std::string, shared_ptr<InteractiveTask<uint32_t, float>>> InteractiveTasks;
 /**
  * Main function for the current project
 */
@@ -72,6 +73,7 @@ void tasks_from_config_impl(workflow<float> &work_flow)
 {
     auto ForMainPQ = std::make_unique<PQueue<PriorityQueueElement>>();
     work_flow.load_work();
+
     for (auto item: work_flow.get_tasks()){
         auto net_func = item.second;
         std::cout<<net_func->task_name<<"\n";
@@ -100,19 +102,9 @@ void tasks_from_config_impl(workflow<float> &work_flow)
         NetFunctionFactory::supportNetFunction(net_action.second.action);
     }
 
-    std::vector<std::string> routines{"Main", "Motors", "Position"};
-    for (auto routine: routines)
-    {
-        queues_map.emplace(routine,
-                           std::map<std::string, shared_ptr_thread_safe_sample_queue>
-                           {{"in",  std::make_shared<thread_safe_sample_queue>()},
-                            {"out", std::make_shared<thread_safe_sample_queue>()}});
-
-    }
-
     const auto &action_items = work_flow.get_net_actions();
     const std::map<std::string, std::shared_ptr<NetFunction>> &net_functions = NetFunctionFactory::get_active_net_functions();
-    //net_functions["roll"]->set_motors();
+
     for (auto &[net_function_name, net_action_details]: action_items)
     {
         auto net_func = net_functions.at(net_action_details.action);
@@ -123,21 +115,47 @@ void tasks_from_config_impl(workflow<float> &work_flow)
         cout<<net_action_details.action<<"\n";
     }
 
-
+    utils::DB<uint32_t, float> db;
+    std::map<uint32_t, float> new_engines;
+    for (auto motor_instance: work_flow.get_all_motors())
+    {
+        new_engines[motor_instance] = 0.5f;
+    }
+    db.set_component_db("Engines", new_engines);
+    db.set_component_db("PositionLoop", std::map<uint32_t, float>{{1, 0.5f}});
 
 
     auto end_task = std::make_shared<std::atomic<bool>>(false);
 
-
-
     auto net_func_callback = std::bind(&execute_net_function, net_functions, std::placeholders::_1, std::placeholders::_2);
-    InteractiveTasks.emplace("Main", std::make_shared<InteractiveTask>(std::move(std::string("Main")),queues_map["Main"]["in"], queues_map["Motors"]["in"], std::move(ForMainPQ), end_task, 100, net_func_callback));
 
-    //InteractiveTasks.emplace("Main", std::make_shared<InteractiveTask>(std::move(std::string("Main")),queues_map["Main"]["in"], queues_map["Motors"]["in"], std::move(nullptr), end_task, 100, net_func_callback));
-    InteractiveTasks.at("Main")->inject_elements_to_queue(work_flow.get_net_func_motors());
-    InteractiveTasks.emplace("Motor", std::make_shared<InteractiveTask>(std::move(std::string("Motor")), queues_map["Motors"]["in"], queues_map["Main"]["in"], nullptr, end_task, 10, execute_auto_tune_function));
+
+    /**********/
+    //  main  //
+    /**********/
+    //won't bind, main can get db both for Engines and for PositionLoop
+    auto get_db_values_callback = std::bind(&utils::DB<uint32_t,float>::get_component_db, &db, std::placeholders::_1);
+
+    //can bind, loops does not set value
+    auto engines_set_db_values_callback = std::bind(&utils::DB<uint32_t,float>::set_component_db, &db, std::string("Engines"), std::placeholders::_2);
+    //auto engines_set_db_values_callback = std::bind(&utils::DB<uint32_t,float>::set_component_db, &db, std::placeholders::_1, std::placeholders::_2);
+    InteractiveTasks.emplace("Main", std::make_shared<InteractiveTask<uint32_t, float>>(std::move(std::string("Main")), std::move(ForMainPQ), end_task, 100, net_func_callback, get_db_values_callback, engines_set_db_values_callback));
+    InteractiveTasks.emplace("Main", std::make_shared<InteractiveTask<uint32_t, float>>(engines_set_db_values_callback));
+    /**********/
+    //Engines //
+    /**********/
+    auto engines_get_db_values_callback = std::bind(&utils::DB<uint32_t,float>::get_component_db, &db, std::string("Engines"));
+    InteractiveTasks.emplace("Engines", std::make_shared<InteractiveTask<uint32_t, float>>(std::move(std::string("Engines")), std::move(ForMainPQ), end_task, 100, net_func_callback, engines_get_db_values_callback, engines_set_db_values_callback));
+    /****************/
+    // PositionLoop //
+    /****************/
+    //auto loop_set_db_values_callback = std::bind(&utils::DB<uint32_t,float>::set_component_db, &db, std::placeholders::_1, std::string("position_loop"));
+    //InteractiveTasks.emplace("PositionLoop", std::make_shared<InteractiveTask<uint32_t, float>>(std::move(std::string("Engines")), std::move(ForMainPQ), end_task, 100, net_func_callback, engines_get_db_values_callback, engines_set_db_values_callback));
+
 
     std::this_thread::sleep_for(500000ms);
+
     end_task->store(true);
+
 
 }
